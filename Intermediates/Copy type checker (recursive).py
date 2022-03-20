1fe19f3f382fd5f7ae43fc3f7a9114268b0f2154
Frank17 <83507659+Frank17@users.noreply.github.com>
2022-03-19 18:29:37 -0700

    Update Copy type checker (recursive).py

--- Intermediates/Copy type checker (recursive).py
+++ Intermediates/Copy type checker (recursive).py
@@ -1,4 +1,5 @@
 from collections.abc import MutableSequence, MutableSet, MutableMapping, Container
+from copy import deepcopy
 
 
 class NoMutableElementFoundError(Exception): pass
@@ -12,36 +13,46 @@ def is_container(obj):
     return isinstance(obj, Container)
 
 
+def get_type(i, j):
+    return 'shallow copy' if i is j else 'deep copy'
+
+
 class Checker:
     def __init__(self, iter1, iter2):
         assert iter1 == iter2, 'Two iterables should have the same value'
-        self.iter1 = iter1
-        self.iter2 = iter2
+        self._iter1 = iter1
+        self._iter2 = iter2
+        self.iter1 = deepcopy(iter1)
+        self.iter2 = deepcopy(iter2)
+        self.stop_at = None
         self.has_mutable = False
 
-    def check_copy(self, recursive=False):
-        if self.iter1 is self.iter2:
+    def check_copy(self, recursive=False, is_first=True):
+        if is_first and (self._iter1 is self._iter2):
             return 'assignment'
-        elif isinstance(self.iter1, dict):
-            return self._recursive(self.iter1.values(),
-                                   self.iter2.values(), recursive)
-        return self._recursive(self.iter1, self.iter2, recursive)
+        elif isinstance(self._iter1, dict):
+            return self._recursive(self._iter1.values(),
+                                   self._iter2.values(), recursive)
+        return self._recursive(self._iter1, self._iter2, recursive)
 
     def _recursive(self, iter1, iter2, recursive):
         if recursive:
             for i, j in zip(iter1, iter2):
                 if is_mutable(i):
                     self.has_mutable = True
+                    self.stop_at = i
+                    iter1, iter2 = i, j
                     break
                 if is_container(i):
-                    self.iter1, self.iter2 = i, j
-                    self.check_copy(True)
-        return self._get_type(self.iter1, self.iter2)
+                    self._iter1, self._iter2 = i, j
+                    return self.check_copy(True, False)
+        return self._get_type(iter1, iter2)
 
     def _get_type(self, iter1, iter2):
         if self.has_mutable:
-            return 'shallow copy' if iter1 is iter2 else 'deep copy'
+            return get_type(iter1, iter2)
         for i, j in zip(iter1, iter2):
             if is_mutable(i):
-                return 'shallow copy' if i is j else 'deep copy'
+                self.stop_at = i
+                return get_type(i, j)
         raise NoMutableElementFoundError
